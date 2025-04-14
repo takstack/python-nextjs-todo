3ca3f57734945d28209754fce9e17a10272c748a
randy <randy.takaki@gmail.com>
2025-04-13 20:36:29 -0700

    add args to dockerfile

--- app/lib/appwrite.ts
+++ app/lib/appwrite.ts
@@ -4,8 +4,15 @@ const client = new Client()
 
 console.log('appwrite key', process.env.APPWRITE_URL)
 
-client.setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_URL!)
-client.setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!)
+try {client.setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_URL!)}
+catch (e){
+    console.log("setendpoint err: ", e.message)
+}
+
+try {client.setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!)}
+catch (e){
+    console.log("setproject err: ", e.message)
+}
 
 export const account = new Account(client);
 
